9020f02af72355257baed9e4223d41c01195d76d
pooyamollahasani-create <pooyamollahasani@gmail.com>
2026-08-29 11:56:10 +0330

    Keep BTC member invites disabled

--- bot/handlers/moderation.py
+++ bot/handlers/moderation.py
@@ -45,7 +45,7 @@ def _open_permissions() -> ChatPermissions:
         can_send_other_messages=True,
         can_add_web_page_previews=True,
         can_change_info=False,
-        can_invite_users=True,
+        can_invite_users=False,
         can_pin_messages=False,
         can_manage_topics=False,
     )
